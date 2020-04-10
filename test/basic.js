29aed76250675cc3fcb12a41c8dbf4e041998664
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-04-10 13:55:17 -0700

    added duh2svd to the index

--- test/basic.js
+++ test/basic.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const chai = require('chai');
-const duh2svd = require('../lib/duh2svd.js');
+const dut = require('../lib/');
 
 const expect = chai.expect;
 
 describe('basic', () => {
 
   it('one', done => {
-    expect(duh2svd).to.be.a('function');
+    expect(dut.duh2svd).to.be.a('function');
     done();
   });
 
